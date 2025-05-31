6f2a2668d169e61cdc41045ec8deb9116c536c4d
Connor Frunzi <connor.frunzi@icloud.com>
2025-05-31 11:00:29 -0400

    added cart actions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload; // destructure product details from action payload
+        // check if item already exists in the cart
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem){
+            // increment quantity if item exists
+            existingItem.quantity++;
+        } else {
+            // if item doesn't exist in the cart, add new item to array via push
+            state.items.push({name, image, cost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const {name, quantity} = action.payload;
 
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate){
+            itemToUpdate.quantity = quantity;
+        }
     
     },
   },
